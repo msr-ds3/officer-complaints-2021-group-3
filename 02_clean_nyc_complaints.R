db8f0b970d557c615e78abea6479163aa6969e8d
Anthony Vallejo <mctony5151@gmail.com>
2021-06-22 16:15:11 -0400

    R file updated

--- 02_clean_nyc_complaints.R
+++ 02_clean_nyc_complaints.R
@@ -7,11 +7,16 @@ library(ggplot2)
 
 nyc_office_data <- read.csv("Documents/officer-complaints-group-3/CCRB%20Complaint%20Database%20Raw%2004.20.2021.csv")
 
-df1 <- nyc_office_data %>% group_by(OfficerID) %>% summarize(Complaints = n()) %>% 
-  arrange(Complaints) %>% mutate(complain_sum = cumsum(Complaints)/sum(Complaints)) %>% 
-  summarize(cum_sum = quantile(complain_sum, probs = seq(0,1, 0.1)))
+df1 <- nyc_office_data %>% group_by(ComplaintID,OfficerID) %>% summarize(Complaints = n()) 
+#%>% arrange(Complaints) %>% mutate(complain_sum = cumsum(Complaints)/sum(Complaints)) %>% 
+#summarize(cum_sum = quantile(complain_sum, probs = seq(0,1, 0.1)))
   
+df2 <- df1 %>% group_by(OfficerID) %>% summarize(Complaints_number = n()) %>% arrange(Complaints_number) %>% 
+  mutate(complain_sum = cumsum(Complaints_number)/sum(Complaints_number)) %>% 
+  summarize(cum_sum = quantile(complain_sum, probs = seq(0,1, 0.1)))
 
-ggplot(df1, aes(y = cum_sum)) +
+percent_of_complaints <- data.frame(diff(as.matrix(df2)))
+
+ggplot(percent_of_complaints, aes(y = cum_sum)) +
   geom_histogram() + stat_bin(bins = 10)
 
